41d205683526a1909454cbe5df196e603d962a33
franciscoeluna <franciscoezequielluna1@gmail.com>
2023-04-15 12:40:59 -0300

    nueva db

--- Blog1/settings.py
+++ Blog1/settings.py
@@ -92,16 +92,10 @@ WSGI_APPLICATION = 'Blog1.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-     "default": {
-         "ENGINE": "django.db.backends.sqlite3",
-         "NAME": BASE_DIR / 'db.sqlite3',
-    }
-
-    # 'default': dj_database_url.config(
-    #      default='postgresql://postgres:postgres@localhost:5432/mysite',        
-    #      conn_max_age=600)
+     'default': dj_database_url.config(
+          default='postgresql://postgres:postgres@localhost:5432/mysite',        
+          conn_max_age=600)
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
