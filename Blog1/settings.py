bf7df266ac64fc3eab0e9d569f50fc96e2895419
franciscoeluna <franciscoezequielluna1@gmail.com>
2023-04-16 15:55:14 -0300

    nuevo2

--- Blog1/settings.py
+++ Blog1/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
-import dj_database_url
+#import dj_database_url
 
 import whitenoise
 
@@ -92,9 +92,10 @@ WSGI_APPLICATION = 'Blog1.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-     'default': dj_database_url.config(
-          default='postgresql://postgres:postgres@localhost:5432/mysite',        
-          conn_max_age=600)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
